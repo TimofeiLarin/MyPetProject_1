b9835d596822d3baf46d2c6f63e6016b501f13f9
Timofei Larin <timofeilarine31@gmail.ru>
2021-12-19 01:12:05 +0300

    Code refactoring

--- script.js
+++ script.js
@@ -3,90 +3,87 @@ let taskText = document.querySelector('.task-text');
 let important = document.querySelector('.important');
 let taskAdd = document.querySelector('.task-add');
 
-let Tasks = [];
+let tasks = [];
+
+if (localStorage.getItem('todo')){
+  tasks = JSON.parse(localStorage.getItem('todo'));
+  addTasks();
+}
 
 taskAdd.addEventListener('click', function() {
-  
+  if(taskText.value === '') return;
+
   let newTask = {
     task: taskText.value,
     complete: false,
     important: false,
   };
 
-  Tasks.push(newTask);
+  if (important.checked) {
+    tasks.unshift(newTask);
+    newTask.important = true;
+  } else {
+    tasks.push(newTask);
+  }
 
-  if(taskText.value === '') return;
-  addDeleteTasks();
-
+  addTasks();
+  localStorage.setItem('todo', JSON.stringify(tasks));
   taskText.value = '';
+  important.checked = false;
 });
 
-
-
-function addDeleteTasks() {
-
+function addTasks() {
   let addTask = '';
-  
-  Tasks.forEach(function(item, index) {
-  
-    addTask += `
-    <li class='task ${item.complete ? 'task-complete' : ''}'> ${item.task}
-    <button class="btn-complete" title="Mission complete">Complete</button>
-    <button class="btn-delete" title="Delete task">Delete</button>
-    </li>
-    `;
-    
+
+  if (tasks.length === 0) checkList.innerHTML = '';
+
+  tasks.forEach(function(item, index) {
+    addTask += `<li class="task ${item.complete ? 'task-complete' : ''} ${item.important ? 'task-important' : ''} item_c${index}" id="item_d${index}">${item.task}<button class="btn-complete" id="item_c${index}" onclick="completeTask(${index})" title="Mission complete">Complete</button><button class="btn-delete" id="item_d${index}" onclick="deleteTask(${index})" title="Delete task">Delete</button></li>`;
     checkList.innerHTML = addTask;
-
-    
-    })
-
-    Tasks.forEach(function(item, index) {
-      let buttonCompleteTask = document.querySelector('.btn-complete');
-
-    
-      buttonCompleteTask.addEventListener('click', function() {
-        item.complete = !item.complete;
-        console.log(item.complete);
-    })
-
-  });
-    
-/*
-    let task = document.createElement('li');
-    let buttonCompleteTask = document.createElement('button');
-    let buttonDeleteTask = document.createElement('button');
-    
-    task.className = 'task'; 
-    task.textContent = item.task;
-    if (important.checked) {
-      checkList.prepend(task);
-      task.classList.add('task-important');
-    } else {
-      checkList.appendChild(task);
-    }
-*/
-    /*
-    buttonCompleteTask.className = 'btn-complete';
-    buttonCompleteTask.textContent = 'Complete';
-    buttonCompleteTask.title = 'Mission complete';
-    task.appendChild(buttonCompleteTask);
-  
-    buttonDeleteTask.className = 'btn-delete';
-    buttonDeleteTask.textContent = 'Delete';
-    buttonDeleteTask.title = "Delete task";
-    task.appendChild(buttonDeleteTask);
-    */
-    /*
-    buttonDeleteTask.addEventListener('click', function() {
-      checkList.removeChild(task);
     });
-  
-    buttonCompleteTask.addEventListener('click', function() {
-      task.classList.toggle('task-complete');
-    })
-    */
-  
+  };
 
+  function completeTask(index) {
+    tasks[index].complete = !tasks[index].complete;
+    console.log(index);
+    let completeTask = tasks.filter(item => item.complete === true);
+    let acticeTask= tasks.filter(item => item.complete === false);
+    tasks = [...acticeTask,...completeTask];
+    addTasks();
+    localStorage.setItem('todo', JSON.stringify(tasks));
+  }
 
-}
+  function deleteTask(index) {
+    tasks.splice(index, 1);
+    console.log(index);
+    addTasks();
+    localStorage.setItem('todo', JSON.stringify(tasks));
+  }
+
+/*
+   checkList.addEventListener('click', function(event) {
+    let idComplete = event.target.getAttribute('id');
+    let taskComplete = document.querySelector('.' + idComplete).childNodes[0].nodeValue;
+    Tasks.forEach( function(item) {
+      if (item.task === taskComplete) {
+        item.complete = !item.complete;
+        addTasks();
+        localStorage.setItem('todo', JSON.stringify(Tasks));
+      }
+    });
+  });
+*/
+
+/*
+  checkList.addEventListener('click', function(event) {
+    let idDelete = event.target.getAttribute('id');
+    let taskDelete = checkList.querySelector('#' + idDelete).childNodes[0].nodeValue;
+    Tasks.forEach( function(item, index) {
+      if (item.task === taskDelete) {
+        Tasks.splice(index, 1);
+        addTasks();
+        localStorage.setItem('todo', JSON.stringify(Tasks));
+      }
+    });
+  });
+  */
