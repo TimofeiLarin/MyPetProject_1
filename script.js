1043b30d086ba5b70e78464ceea7c7221828c683
Timofei Larin <timofeilarine31@gmail.ru>
2022-02-17 16:26:55 +0300

    Refactoring JS, HTML, CSS, added the feature of adding a task by pressing Enter, added markup adaptability

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let checkList = document.querySelector('.check-list');
 let taskText = document.querySelector('.task-text');
 let important = document.querySelector('.important');
 let taskAdd = document.querySelector('.task-add');
-
 let tasks = [];
 
 if (localStorage.getItem('todo')){
@@ -10,80 +9,61 @@ if (localStorage.getItem('todo')){
   addTasks();
 }
 
-taskAdd.addEventListener('click', function() {
+function createTask() {
   if(taskText.value === '') return;
-
   let newTask = {
     task: taskText.value,
     complete: false,
     important: false,
   };
-
   if (important.checked) {
     tasks.unshift(newTask);
     newTask.important = true;
   } else {
-    tasks.push(newTask);
-  }
-
+      tasks.push(newTask);
+    }
   addTasks();
   localStorage.setItem('todo', JSON.stringify(tasks));
   taskText.value = '';
   important.checked = false;
+};
+
+taskAdd.addEventListener('click' , createTask);
+document.addEventListener('keydown' , e => {
+  if (e.keyCode === 13) createTask();
 });
+
 
 function addTasks() {
   let addTask = '';
-
   if (tasks.length === 0) checkList.innerHTML = '';
-
   tasks.forEach(function(item, index) {
-    addTask += `<li class="task ${item.complete ? 'task-complete' : ''} ${item.important ? 'task-important' : ''} item_c${index}" id="item_d${index}">${item.task}<button class="btn-complete" id="item_c${index}" onclick="completeTask(${index})" title="Mission complete">Complete</button><button class="btn-delete" id="item_d${index}" onclick="deleteTask(${index})" title="Delete task">Delete</button></li>`;
+    addTask += `
+              <div class = "container-task">
+                <li class="task ${item.complete ? 'task-complete' : ''} ${item.important ? 'task-important' : ''} item_c${index}" id="item_d${index}">
+                  ${item.task}
+                </li>
+                <div class="container-btn">
+                  <button class="btn-complete" id="item_c${index}" onclick="completeTask(${index})" title="Mission complete">Complete</button>
+                  <button class="btn-delete" id="item_d${index}" onclick="deleteTask(${index})" title="Delete task">Delete</button>
+                </div>
+              </div>
+            `;
     checkList.innerHTML = addTask;
-    });
-  };
-
-  function completeTask(index) {
-    tasks[index].complete = !tasks[index].complete;
-    console.log(index);
-    let completeTask = tasks.filter(item => item.complete === true);
-    let acticeTask= tasks.filter(item => item.complete === false);
-    tasks = [...acticeTask,...completeTask];
-    addTasks();
-    localStorage.setItem('todo', JSON.stringify(tasks));
-  }
-
-  function deleteTask(index) {
-    tasks.splice(index, 1);
-    console.log(index);
-    addTasks();
-    localStorage.setItem('todo', JSON.stringify(tasks));
-  }
-
-/*
-   checkList.addEventListener('click', function(event) {
-    let idComplete = event.target.getAttribute('id');
-    let taskComplete = document.querySelector('.' + idComplete).childNodes[0].nodeValue;
-    Tasks.forEach( function(item) {
-      if (item.task === taskComplete) {
-        item.complete = !item.complete;
-        addTasks();
-        localStorage.setItem('todo', JSON.stringify(Tasks));
-      }
-    });
   });
-*/
+};
 
-/*
-  checkList.addEventListener('click', function(event) {
-    let idDelete = event.target.getAttribute('id');
-    let taskDelete = checkList.querySelector('#' + idDelete).childNodes[0].nodeValue;
-    Tasks.forEach( function(item, index) {
-      if (item.task === taskDelete) {
-        Tasks.splice(index, 1);
-        addTasks();
-        localStorage.setItem('todo', JSON.stringify(Tasks));
-      }
-    });
-  });
-  */
+function completeTask(index) {
+  tasks[index].complete = !tasks[index].complete;
+  let completeTask = tasks.filter(item => item.complete === true);
+  let activeTask= tasks.filter(item => item.complete === false);
+  tasks = [...activeTask,...completeTask];
+  addTasks();
+  localStorage.setItem('todo', JSON.stringify(tasks));
+}
+
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  addTasks();
+  localStorage.setItem('todo', JSON.stringify(tasks));
+}
